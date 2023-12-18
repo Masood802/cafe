9b47e7faadd2efe5cfbcb7b85776a054120e5081
Masood <masoodsse@gmail.com>
2023-12-18 15:54:15 +0500

    cleaning

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,28 +3,5 @@ import navbar from "@/components/navbar.vue";
 </script>
 <template>
   <navbar></navbar>
-  <div
-    id="about"
-    class="w-4/5 h-[450px] mx-20 flex justify-center items-center my-12"
-  >
-    <div class="w-1/2">
-      <p class="text-xs text-gray-400 my-12">READ OUR STORY</p>
-      <h2 class="w-full text-4xl font-bold text-gray-800 leading-normal">
-        We've been Making The<br />
-        Delicious Foods Since 1999
-      </h2>
-      <p class="w-full py-3">
-        Fusce hendrerit malesuada lacinia. Donec semper semper sem vitae
-        malesuada. Proin scelerisque risus et ipsum semper molestie sed in nisi.
-        Ut rhoncus congue lectus, rhoncus venenatis leo malesuada id. Sed
-        elementum vel felis sed scelerisque. In arcu diam, sollicitudin eu nibh
-        ac, posuere tristique magna. You can use this template for your cafe or
-        restaurant website. Please tell your friends about templatemo. Thank
-        you.
-      </p>
-    </div>
-    <div class="w-2/3 m-2 p-2">
-      <img src="/about-image.jpg" alt="aboutImage" />
-    </div>
-  </div>
+  
 </template>
