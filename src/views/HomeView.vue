dcf1f0039625bd838ca59cb240789804c51309f3
Masood <masoodsse@gmail.com>
2023-12-18 16:35:07 +0500

    new images added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -54,23 +54,62 @@ import navbar from "@/components/navbar.vue";
     <div class="w-3/5 mx-auto flex justify-center items-center">
       <div class="w-1/3 mx-2 flex flex-col justify-center items-center">
         <img src="/team-image1.jpg" alt="" />
-        <h2 class="font-bold text-center text-xl pt-4 text-gray-700">New Catherine</h2>
-        <p class="font-bold text-center pt-1 pb-4 text-gray-700">KITCHEN OFFICER</p>
+        <h2 class="font-bold text-center text-xl pt-4 text-gray-700">
+          New Catherine
+        </h2>
+        <p class="font-bold text-center pt-1 pb-4 text-gray-700">
+          KITCHEN OFFICER
+        </p>
       </div>
       <div class="w-1/3 mx-2 flex flex-col justify-center items-center">
         <img src="/team-image2.jpg" alt="" />
-        <h2 class="font-bold text-center text-xl pt-4 text-gray-700">Lindsay Perlen</h2>
-        <p class="font-bold text-center pt-1 pb-4 text-gray-700">owner & manger</p>
+        <h2 class="font-bold text-center text-xl pt-4 text-gray-700">
+          Lindsay Perlen
+        </h2>
+        <p class="font-bold text-center pt-1 pb-4 text-gray-700">
+          owner & manger
+        </p>
       </div>
       <div class="w-1/3 mx-2 flex flex-col justify-center items-center">
         <img src="/team-image3.jpg" alt="" />
-        <h2 class="font-bold text-center text-xl pt-4 text-gray-700">Isabella Grace</h2>
-        <p class="font-bold text-center pt-1 pb-4 text-gray-700">Pizza specialist</p>
+        <h2 class="font-bold text-center text-xl pt-4 text-gray-700">
+          Isabella Grace
+        </h2>
+        <p class="font-bold text-center pt-1 pb-4 text-gray-700">
+          Pizza specialist
+        </p>
       </div>
     </div>
   </div>
-  <div class="w-full h-auto my-20 ">
-    <h1 class="text-center text-4xl font-bold text-gray-800 mb-4 mt-6  ">Our Menus</h1>
+  <div class="w-full h-auto my-20">
+    <h1 class="text-center text-4xl font-bold text-gray-800 mb-4 mt-6">
+      Our Menus
+    </h1>
+    <p class="text-center uppercase text-slate-400 font-bold mb-5">
+      tea time & dining
+    </p>
+    <div class="w-[99%] flex flex-wrap mx-6 group">
+      <div
+        class="bg-gray-400 absolute my-72 w-[33%] h-28 opacity-0 z-50 group-hover:opacity-100 transition-all duration-700 ease-in-out"
+      >
+        <p class="text-xl font-bold px-2 py-4 text-slate-50">
+          American breakfast <span class="pl-52">25$</span>
+        </p>
+        <h3 class="font-bold mx-2">Tomato / eggs / sausages</h3>
+      </div>
+      <div class="w-1/3">
+        <img
+          src="/menu-image1.jpg"
+          alt="img"
+          class=" w-full scale-100 hover:scale-110 transition-all duration-300 ease-linear object-cover z-0 hover:overflow-hidden"
+        />
+      </div>
 
+      <img src="/menu-image2.jpg" alt="img" />
+      <img src="/menu-image3.jpg" alt="img" />
+      <img src="/menu-image4.jpg" alt="img" />
+      <img src="/menu-image5.jpg" alt="img" />
+      <img src="/menu-image6.jpg" alt="img" />
+    </div>
   </div>
 </template>
