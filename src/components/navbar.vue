41868080b711c511b5970f269431d1e4f0c738e5
Masood <masoodsse@gmail.com>
2023-12-18 13:21:32 +0500

    loaded

---
+++ src/components/navbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="w-full font-bold">
+    <router-link to="/" class="px-3">HOME </router-link>
+    <router-link to="#about" class="px-3">ABOUT </router-link>
+    <router-link to="/" class="px-3">CHEF </router-link>
+    <router-link to="/" class="px-3">MENU </router-link>
+    <router-link to="/" class="px-3">CONTACT </router-link>
+  </div>
+</template>
