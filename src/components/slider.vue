2fe6c8605cf3fd3f6cd41bd45cba382264e6400d
Masood <masoodsse@gmail.com>
2023-12-21 12:57:05 +0500

    cleaning

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -7,49 +7,18 @@ let images = ref([
 ]);
 let i = ref(0);
 onMounted(() => {
-  document.getElementById("div1").style.backgroundColor = "White";
   setInterval(() => {
-    if (i.value == 0 || i.value <= 2) {
-      if (i.value == 0) {
-        document.getElementById("div1").style.backgroundColor = "White";
-        document.getElementById("div2").style.backgroundColor = "Transparent";
-        document.getElementById("div3").style.backgroundColor = "Transparent";
-      }
-      i.value = i.value + 1;
-      if (i.value == 1) {
-        document.getElementById("div2").style.backgroundColor = "White";
-        document.getElementById("div1").style.backgroundColor = "Transparent";
-        document.getElementById("div3").style.backgroundColor = "Transparent";
-      }
-      if (i.value == 2) {
-        document.getElementById("div3").style.backgroundColor = "White";
-        document.getElementById("div2").style.backgroundColor = "Transparent";
-        document.getElementById("div1").style.backgroundColor = "Transparent";
-      }
-      if (i.value == 3) {
-        i.value = 0;
-        document.getElementById("div1").style.backgroundColor = "White";
-        document.getElementById("div2").style.backgroundColor = "Transparent";
-        document.getElementById("div3").style.backgroundColor = "Transparent";
-      }
+    i.value = i.value + 1;
+    if (i.value == 3) {
+      i.value = 0;
     }
-  }, 10*1000);
+  }, 10 * 1000);
 });
 </script>
 <template>
-  <div class="w-1/2 my-10 mx-auto relative">
-    <img :src="images[i]" alt="" />
-    <div
-      class="w-3 h-3 rounded-full border border-gray-50 absolute bottom-5 left-[45%]"
-      id="div1"
-    ></div>
-    <div
-      class="w-3 h-3 rounded-full border border-gray-50 absolute bottom-5 left-[50%]"
-      id="div2"
-    ></div>
-    <div
-      class="w-3 h-3 rounded-full border border-gray-50 absolute bottom-5 left-[55%]"
-      id="div3"
-    ></div>
+  <div
+    class="w-full h-[700px]  overflow-auto"
+  >
+      <img :src="images[i]" alt="" class="w-full object-cover" />
   </div>
 </template>
